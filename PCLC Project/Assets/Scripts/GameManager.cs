587adf387a112249bc161622e71a2a8cb1af8cd5
ChrisQube <gameQube@outlook.com>
2020-04-15 16:36:16 +1200

    Text reveal on side card movement

--- PCLC Project/Assets/Scripts/GameManager.cs
+++ PCLC Project/Assets/Scripts/GameManager.cs
@@ -12,10 +12,16 @@ public class GameManager : MonoBehaviour
 
     public float fMovingSpeed;
     Vector3 pos;
-    public TextMeshProUGUI display;
+
+    public TextMeshProUGUI display; //for debugging
+    public TextMeshPro mainText;
+    public TextMeshPro upText;
+    public TextMeshPro downText;
+
+    public float f_xDistanceToMargin;
+    public float f_yDistanceToMargin;
 
     public float f_xDistanceToTrigger;
-    public float f_yDistanceToTrigger;
 
     void Start()
     {
@@ -36,34 +42,54 @@ public class GameManager : MonoBehaviour
         }
         display.text = "x = " + cardGameObject.transform.position.x.ToString() + "; y = " + cardGameObject.transform.position.y.ToString();
 
+        
+        
         ////Checking sides
-        //Right Side + Up
-        if (cardGameObject.transform.position.x > f_xDistanceToTrigger && cardGameObject.transform.position.y < -f_yDistanceToTrigger)
+        //Right Side +/- Down
+        if (cardGameObject.transform.position.x > 0)//f_xDistanceToMargin) // && cardGameObject.transform.position.y < -f_yDistanceToTrigger)
         {
-            
+            byte alphaValue = (byte)(Mathf.Min(cardGameObject.transform.position.x, 1) * 255);
+            downText.faceColor = new Color32(0, 0, 0, alphaValue);
+            //Debug.Log("a = " + Mathf.Min(cardGameObject.transform.position.x, 1));
+            Debug.Log("alpha = " + alphaValue);
             //display.text = "x = " + cardGameObject.transform.position.x.ToString() + "; y = " + cardGameObject.transform.position.y.ToString();
-            cardSpriteRenderer.color = Color.green;
+            //cardSpriteRenderer.color = Color.green;
 
-            if (!Input.GetMouseButton(0))
-            {
-                //mainCardController.InduceRight();
-            }
+            mainText.text = alphaValue.ToString();
         }
-        //Left Side + Down
-        else if (cardGameObject.transform.position.x < -f_xDistanceToTrigger && cardGameObject.transform.position.y > f_yDistanceToTrigger) 
+        //Left Side +/- Up
+        else if (cardGameObject.transform.position.x < 0)//-f_xDistanceToMargin) // && cardGameObject.transform.position.y > f_yDistanceToTrigger) 
         {
 
+            byte alphaValue = (byte)(Mathf.Min(-cardGameObject.transform.position.x, 1) * 255);
+            upText.faceColor = new Color32(0, 0, 0, alphaValue);
+            //downText.alpha = Mathf.Min(-cardGameObject.transform.position.x, 1);
+            //Debug.Log("a = " + Mathf.Min(-cardGameObject.transform.position.x, 1));
             //display.text = "x = " + cardGameObject.transform.position.x.ToString() + "; y = " + cardGameObject.transform.position.y.ToString();
-            cardSpriteRenderer.color = Color.red;
+            //cardSpriteRenderer.color = Color.red;
 
-            if (!Input.GetMouseButton(0))
-            {
-                //mainCardController.InduceLeft();
-            }
+            mainText.text = alphaValue.ToString();
         }
         else
         {
-            cardSpriteRenderer.color = Color.white;
+            upText.faceColor = new Color32(0, 0, 0, 50);
+            downText.faceColor = new Color32(0, 0, 0, 50);
+            //cardSpriteRenderer.color = Color.white;
         }
     }
+
+    private void OnMouseUp()
+    {
+        if (!Input.GetMouseButton(0) && cardGameObject.transform.position.x > f_xDistanceToTrigger)
+        {
+            //mainCardController.InduceRight();
+            Debug.Log("Right");
+        }
+        else if(!Input.GetMouseButton(0) && cardGameObject.transform.position.x < -f_xDistanceToTrigger)
+        {
+            //mainCardController.InduceLeft();
+            Debug.Log("Left");
+        }
+    }
+
 }
