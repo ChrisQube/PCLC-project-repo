799858af0bcccdbe12c1f86d3a90a7aee51e9286
ChrisQube <gameQube@outlook.com>
2020-04-15 14:49:34 +1200

    Added corner swiping update

---
+++ PCLC Project/Assets/Scripts/CardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardController : MonoBehaviour
+{
+    public BoxCollider2D thisCardBoxCollider2D;
+    public bool isMouseOver;
+
+    private void Start()
+    {
+        thisCardBoxCollider2D = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    private void OnMouseOver()
+    {
+        isMouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isMouseOver = false;   
+    }
+}
---
+++ PCLC Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject cardGameObject;
+    public SpriteRenderer cardSpriteRenderer;
+
+    public CardController mainCardController;
+
+    public float fMovingSpeed;
+    Vector3 pos;
+    public TextMeshProUGUI display;
+
+    public float f_xDistanceToTrigger;
+    public float f_yDistanceToTrigger;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       //Movement
+       if (Input.GetMouseButton(0) && mainCardController.isMouseOver)
+       {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            cardGameObject.transform.position = pos;
+       }
+       else
+        {
+            cardGameObject.transform.position = Vector2.MoveTowards(cardGameObject.transform.position, new Vector2(0, 0), fMovingSpeed);
+        }
+        display.text = "x = " + cardGameObject.transform.position.x.ToString() + "; y = " + cardGameObject.transform.position.y.ToString();
+
+        ////Checking sides
+        //Right Side + Up
+        if (cardGameObject.transform.position.x > f_xDistanceToTrigger && cardGameObject.transform.position.y < -f_yDistanceToTrigger)
+        {
+            
+            //display.text = "x = " + cardGameObject.transform.position.x.ToString() + "; y = " + cardGameObject.transform.position.y.ToString();
+            cardSpriteRenderer.color = Color.green;
+
+            if (!Input.GetMouseButton(0))
+            {
+                //mainCardController.InduceRight();
+            }
+        }
+        //Left Side + Down
+        else if (cardGameObject.transform.position.x < -f_xDistanceToTrigger && cardGameObject.transform.position.y > f_yDistanceToTrigger) 
+        {
+
+            //display.text = "x = " + cardGameObject.transform.position.x.ToString() + "; y = " + cardGameObject.transform.position.y.ToString();
+            cardSpriteRenderer.color = Color.red;
+
+            if (!Input.GetMouseButton(0))
+            {
+                //mainCardController.InduceLeft();
+            }
+        }
+        else
+        {
+            cardSpriteRenderer.color = Color.white;
+        }
+    }
+}
