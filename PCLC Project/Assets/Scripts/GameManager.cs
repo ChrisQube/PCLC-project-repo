2203276a0f5631fc0123713ce7f41985d5f8c51a
ChrisQube <gameQube@outlook.com>
2020-04-25 19:33:37 +1200

    changed SO to CSV
    Not using scriptable objects anymore. Managed to get import csv to work and allow automatic card selection.

--- PCLC Project/Assets/Scripts/GameManager.cs
+++ PCLC Project/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
@@ -36,12 +37,51 @@ public class GameManager : MonoBehaviour
     public Card currentCard;
     public Card testCard;
 
+    //Card
+    private string[,] cardList = new string[155,11];
+    List<string> stringList = new List<string>();
+    private int currentActiveCardRow;
+
     //Card swipe states
     public enum ChoiceDirection { left, right };
 
+    public enum columnHeading { cardEnum = 0, cardID = 1, ActSection = 2, Main_body_text = 3, LeftOptionText = 4, LeftNextCardID = 5, LeftValue = 6, RightOptionText = 7, RightNextCardID = 8, RightValue = 9, Randomize = 10};
+
     void Start()
     {
-        LoadCard(testCard);
+        readTextFile();
+        currentActiveCardRow = 0;
+        LoadCardFromArray(currentActiveCardRow);
+        //LoadCard(testCard);
+    }
+
+    public void readTextFile()
+    {
+        StreamReader input_Stream = new StreamReader("Assets/Cards/PCLC cards.csv");
+
+        while (!input_Stream.EndOfStream)
+        {
+            string input_line = input_Stream.ReadLine();
+
+            stringList.Add(input_line);
+        }
+        
+        input_Stream.Close();
+
+        //Debug.Log("stringList count = " + stringList.Count);
+
+        //putting input_line -> cardList[,]
+        for (int row = 0; row < stringList.Count; row++)
+        {
+            string[] string_columns = stringList[row].Split('|'); //This is char?
+            for (int col = 0; col < string_columns.Length; col++)
+            {
+                cardList[row, col] = string_columns[col];
+            }
+        }
+
+        //Debug.Log("Array complete.");
+        //Debug.Log("Row 5, col 4 = " + cardList[4,3]);
     }
 
     private float SmoothCardYPosition(float originalY)
@@ -72,6 +112,7 @@ public class GameManager : MonoBehaviour
             //{
             //    pos.y = -0.35f;
             //}
+
             pos.y = SmoothCardYPosition(pos.y);
             cardGameObject.transform.position = pos;
        }
@@ -86,9 +127,11 @@ public class GameManager : MonoBehaviour
         //upText.text = LeftUpQuote;
         //downText.text = RightDownQuote;
         //mainText.text = MainTextQuote;
-        
+
         ////Checking sides
         //Right Side +/- Down
+
+        //Debug.Log("x = " + cardGameObject.transform.position.x);
         if (cardGameObject.transform.position.x > 0)//&& cardGameObject.transform.position.y < 0)
         {
             //ignore
@@ -100,9 +143,12 @@ public class GameManager : MonoBehaviour
             byte XAxisAlphaValue = (byte)(Mathf.Min(cardGameObject.transform.position.x, 1) * 255);
             downText.faceColor = new Color32(0, 0, 0, XAxisAlphaValue);
 
+            //Debug.Log("x is postive");
+
             if (Input.GetMouseButtonUp(0))//if (!Input.GetMouseButton(0) && XAxisAlphaValue > 220)
             {
-                NextCard(ChoiceDirection.right);
+                //NextCard(ChoiceDirection.right); //old
+                NextCardFromArray(ChoiceDirection.right);
                 //Debug.Log("Right");
             }
 
@@ -122,12 +168,15 @@ public class GameManager : MonoBehaviour
             //byte finalAlphaValue = (byte)(Mathf.Max(XAxisAlphaValue, YAxisAlphaValue)); //compares between X and Y for maximum opacity
             //upText.faceColor = new Color32(0, 0, 0, finalAlphaValue);
 
+            //Debug.Log("x is negative");
+
             byte XAxisAlphaValue = (byte)(Mathf.Min(-cardGameObject.transform.position.x, 1) * 255);
             upText.faceColor = new Color32(0, 0, 0, XAxisAlphaValue);
 
             if (Input.GetMouseButtonUp(0))//if (!Input.GetMouseButton(0) && XAxisAlphaValue > 220)
             {
-                NextCard(ChoiceDirection.left);
+                //NextCard(ChoiceDirection.left); //old
+                NextCardFromArray(ChoiceDirection.left);
                 //Debug.Log("Left");
             }
 
@@ -142,8 +191,31 @@ public class GameManager : MonoBehaviour
         {
             upText.faceColor = new Color32(0, 0, 0, 50);
             downText.faceColor = new Color32(0, 0, 0, 50);
+            //Debug.Log("Card not moved.");
             //cardSpriteRenderer.color = Color.white;
         }
+    }
+
+    public void LoadCardFromArray(int cardID)
+    {
+        for (int row = 1; row < cardList.GetLength(0); row++)
+        {
+            if (cardID == int.Parse(cardList[row, (int)columnHeading.cardID])) //this is the right row for the cards! Now we need to load the card.
+            {
+                currentActiveCardRow = row;
+
+                LeftUpQuote = cardList[row, (int)columnHeading.LeftOptionText];
+                RightDownQuote = cardList[row, (int)columnHeading.RightOptionText];
+                MainTextQuote = cardList[row, (int)columnHeading.Main_body_text];
+                //CARD details update when cards load
+                upText.text = LeftUpQuote;
+                downText.text = RightDownQuote;
+                mainText.text = MainTextQuote;
+
+                break;
+            }
+        }
+        Debug.Log("Card has finished loading without quiting the function.");
     }
 
     public void LoadCard(Card card)
@@ -168,6 +240,22 @@ public class GameManager : MonoBehaviour
         //Needs an integer of count, i.e. if the game only has 14 cards, then needs to know which "act/section" the game is in to choose from a different set of cards.
         //Needs randomizer when the "act/section" is known.
             //When specific cardcounts come out; then some cards are "must sees/major life events/decisions"
+    }
+
+    public void NextCardFromArray(ChoiceDirection choice)
+    {
+        Debug.Log("CardID : " + currentActiveCardRow + " swiped " + choice.ToString());
+
+        if (choice == ChoiceDirection.left) 
+        {
+            //load card Left choice
+            LoadCardFromArray(int.Parse(cardList[currentActiveCardRow, (int)columnHeading.LeftNextCardID]));
+        }
+        else 
+        {
+            //load card Right choice
+            LoadCardFromArray(int.Parse(cardList[currentActiveCardRow, (int)columnHeading.RightNextCardID]));
+        }
     }
 
     public void NextCard(ChoiceDirection choice)
