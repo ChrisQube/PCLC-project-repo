1017c599c1186c99681a2c6967f80a99930e56bc
ChrisQube <gameQube@outlook.com>
2020-04-27 18:59:00 +1200

    Added title subtle animation

---
+++ PCLC Project/Assets/Scripts/TitleMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMovement : MonoBehaviour
+{
+    public LeanTweenType easeType;
+    public float fYMovement;
+    public float fYMoveDuration;
+
+    private void OnEnable()
+    {
+        LeanTween.moveLocalY(gameObject, fYMovement, fYMoveDuration).setLoopPingPong().setEase(easeType);
+    }
+}
